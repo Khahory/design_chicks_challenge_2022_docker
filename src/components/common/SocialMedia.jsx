74eab794cc9277bf9816a5eac769248afe9ca04c
Khahory <angeld05@live.com>
2023-02-19 18:37:29 -0400

    social complete and responsive

--- src/components/common/SocialMedia.jsx
+++ src/components/common/SocialMedia.jsx
@@ -1,7 +1,33 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import social_facebook from "../../assets/img/social/facebook.svg";
+import social_instagram from "../../assets/img/social/instagram.svg";
+import social_twitter from "../../assets/img/social/twitter.svg";
+import social_discord from "../../assets/img/social/discord.svg";
+
 const SocialMedia = () => {
+    const col_xs = 1;
+
     return (
-        <div>
-            <h1>SocialMedia</h1>
+        <div className={'background-social'}>
+            <div className='container-main-page'>
+                <Row>
+                    <Col xs="auto">
+                        <img src={social_facebook} alt="social_facebook"/>
+                    </Col>
+                    <Col xs="auto">
+                        <img src={social_instagram} alt="social_instagram"/>
+                    </Col>
+                    <Col xs="auto">
+                        <img src={social_twitter} alt="social_twitter"/>
+                    </Col>
+                    <Col xs="auto">
+                        <img src={social_discord} alt="social_discord"/>
+                    </Col>
+                </Row>
+                <br/>
+                <hr />
+            </div>
         </div>
     )
 }
