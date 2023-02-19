05c161ab817d51c3d93636dc95d2ba4db0d6a642
Khahory <angeld05@live.com>
2023-02-19 18:07:45 -0400

    best wat

--- src/components/common/Banks.jsx
+++ src/components/common/Banks.jsx
@@ -7,23 +7,25 @@ import bank_skrill from "../../assets/img/bank/skrill.svg";
 import bank_crypto from "../../assets/img/bank/crypto.svg";
 
 const Banks = () => {
+    const col_xs = 1;
+
     return (
         <div className={'background-bank'}>
             <div className='container-main-page'>
                 <Row>
-                    <Col xs={1}>
+                    <Col xs={col_xs}>
                         <img src={bank_visa} alt="bank_visa"/>
                     </Col>
-                    <Col xs={1}>
+                    <Col xs={col_xs}>
                         <img src={bank_mastercard} alt='bank_mastercard'/>
                     </Col>
-                    <Col xs={1}>
+                    <Col xs={col_xs}>
                         <img src={bank_amex} alt='bank_amex'/>
                     </Col>
-                    <Col xs={1}>
+                    <Col xs={col_xs}>
                         <img src={bank_skrill} alt='bank_skrill'/>
                     </Col>
-                    <Col xs={1}>
+                    <Col xs={col_xs}>
                         <img src={bank_crypto} alt='bank_crypto'/>
                     </Col>
                     <Col xs={2} >
