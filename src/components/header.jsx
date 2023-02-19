24ebc5d162a394812f2bc05592d09ea36c6789bd
Khahory <angeld05@live.com>
2023-02-19 17:22:08 -0400

    header complete

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,51 +3,66 @@ import Col from 'react-bootstrap/Col';
 import '../assets/css/Header.css';
 import logo from '../assets/img/logo-large.svg'
 import down_arrow from '../assets/icons/arrow-down.svg'
+import btn_add_car from '../assets/img/buttons/btn_add_car.png'
 
 const Header = () => {
+    const height_icon = 6;
     return (
         <div className='nav-bar'>
-            <Row className={'container-page'}>
+            <Row className={'container-page container-page-header'}>
                 <Col className='nav-bar-header-items nav-bar-border'>
                     <img className={'logo'}
                          height={31}
-                         src={logo} alt='logo' />
+                         src={logo} alt='logo'/>
                 </Col>
                 <div className={'content-item'}>
                     <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-page firt-item'>
                         <div>
-                            CURRENCY <img src={down_arrow} alt='down_arrow' height={6} />
+                            CURRENCY <img src={down_arrow} alt='down_arrow' height={height_icon}/>
                         </div>
                     </Col>
                     <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-page'>
                         <div>
-                            ITEMS <img src={down_arrow} alt='down_arrow' height={6} />
+                            ITEMS <img src={down_arrow} alt='down_arrow' height={height_icon}/>
                         </div>
                     </Col>
                     <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-page'>
                         <div>
-                            ACCOUNTS <img src={down_arrow} alt='down_arrow' height={6} />
+                            ACCOUNTS <img src={down_arrow} alt='down_arrow' height={height_icon}/>
                         </div>
                     </Col>
                     <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-page'>
                         <div>
-                            SERVICES <img src={down_arrow} alt='down_arrow' height={6} />
+                            SERVICES <img src={down_arrow} alt='down_arrow' height={height_icon}/>
                         </div>
                     </Col>
                     <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-page'>
                         <div>
-                            SWAP <img src={down_arrow} alt='down_arrow' height={6} />
+                            SWAP <img src={down_arrow} alt='down_arrow' height={height_icon}/>
                         </div>
                     </Col>
                     <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-page'>
                         <div>
-                            SELL <img src={down_arrow} alt='down_arrow' height={6} />
+                            SELL <img src={down_arrow} alt='down_arrow' height={height_icon}/>
                         </div>
                     </Col>
                 </div>
-                <Col className='nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-end'>USD</Col>
-                <Col className='nav-bar-header-items-text nav-bar-header-items'>CAR</Col>
-                <Col className='nav-bar-header-items-text nav-bar-header-items'>SIGN IN</Col>
+                <div className={'content-item'}>
+                    <Col xxl={1} className='aling-item-end nav-bar-header-items-text nav-bar-header-items nav-bar-header-items-end'>
+                        USD <img src={down_arrow} alt='down_arrow' height={height_icon}/>
+                    </Col>
+                    <Col xxl={1} className='aling-item-end nav-bar-header-items-text nav-bar-header-items'>
+                        <div>
+                            <img src={btn_add_car} alt='down_arrow' height={15}/> CART (5)
+                        </div>
+                    </Col>
+                    <Col xxl={1}
+                         className='last-item aling-item-end nav-bar-header-items-text nav-bar-header-items'>
+                        <div className={'box-sigin'}>
+                            SIGN IN <img src={btn_add_car} alt='down_arrow' height={15}/>
+                        </div>
+                    </Col>
+                </div>
             </Row>
         </div>
     );
