e3990daefd851adb77a6900af4696ef6afc5a714
Khahory <angeld05@live.com>
2023-02-19 19:18:34 -0400

    bug fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import logo from "../assets/img/logo-large.svg";
+import '../assets/css/Footer.css'
+
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className={'background-footer'}>
+            <div className='container-main-page'>
+                <Row>
+                    <Col>
+                        <div>
+                            <img className={'logo'}
+                                 height={31}
+                                 src={logo} alt='logo'/>
+                        </div>
+                        <div className={'item-footer'}>support@chicksgold.com</div>
+                    </Col>
+                    <Col>
+                        <div className={'item-footer-text'}><b>Chicks Gold</b></div>
+                        <div className={'item-footer-text'}><b>Games</b></div>
+                        <div className={'item-footer-text'}><b>About Us</b></div>
+                        <div className={'item-footer-text'}><b>Blog</b></div>
+                        <div className={'item-footer-text'}><b>Sitemap</b></div>
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </div>
         </div>
     )
 }
