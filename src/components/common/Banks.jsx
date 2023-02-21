27c97c1e77dae4582ab616a81a52a3f6339e5a69
Khahory <angeld05@live.com>
2023-02-21 00:14:34 -0400

    formatting code

--- src/components/common/Banks.jsx
+++ src/components/common/Banks.jsx
@@ -6,6 +6,7 @@ import bank_mastercard from "../../assets/img/bank/mastercard.svg";
 import bank_amex from "../../assets/img/bank/amex.svg";
 import bank_skrill from "../../assets/img/bank/skrill.svg";
 import bank_crypto from "../../assets/img/bank/crypto.svg";
+import Container from "react-bootstrap/Container";
 
 const Banks = () => {
     const col_xs = 1;
@@ -13,26 +14,28 @@ const Banks = () => {
     return (
         <div className={'background-bank'}>
             <div className='container-main-page'>
-                <Row>
-                    <Col xs={col_xs}>
-                        <img src={bank_visa} alt="bank_visa"/>
-                    </Col>
-                    <Col xs={col_xs}>
-                        <img src={bank_mastercard} alt='bank_mastercard'/>
-                    </Col>
-                    <Col xs={col_xs}>
-                        <img src={bank_amex} alt='bank_amex'/>
-                    </Col>
-                    <Col xs={col_xs}>
-                        <img src={bank_skrill} alt='bank_skrill'/>
-                    </Col>
-                    <Col xs={col_xs}>
-                        <img src={bank_crypto} alt='bank_crypto'/>
-                    </Col>
-                    <Col id='label-more' xs={2} >
-                         <span >and 50+ more</span>
-                    </Col>
-                </Row>
+                <Container>
+                    <Row>
+                        <Col xs={col_xs}>
+                            <img src={bank_visa} alt="bank_visa"/>
+                        </Col>
+                        <Col xs={col_xs}>
+                            <img src={bank_mastercard} alt='bank_mastercard'/>
+                        </Col>
+                        <Col xs={col_xs}>
+                            <img src={bank_amex} alt='bank_amex'/>
+                        </Col>
+                        <Col xs={col_xs}>
+                            <img src={bank_skrill} alt='bank_skrill'/>
+                        </Col>
+                        <Col xs={col_xs}>
+                            <img src={bank_crypto} alt='bank_crypto'/>
+                        </Col>
+                        <Col id='label-more' xs={2}>
+                            <span>and 50+ more</span>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </div>
     )
--- src/components/common/SocialMedia.jsx
+++ src/components/common/SocialMedia.jsx
@@ -4,27 +4,30 @@ import social_facebook from "../../assets/img/social/facebook.svg";
 import social_instagram from "../../assets/img/social/instagram.svg";
 import social_twitter from "../../assets/img/social/twitter.svg";
 import social_discord from "../../assets/img/social/discord.svg";
+import Container from "react-bootstrap/Container";
 
 const SocialMedia = () => {
     return (
         <div className={'background-social'}>
             <div className='container-main-page'>
-                <Row>
-                    <Col xs="auto">
-                        <img src={social_facebook} alt="social_facebook"/>
-                    </Col>
-                    <Col xs="auto">
-                        <img src={social_instagram} alt="social_instagram"/>
-                    </Col>
-                    <Col xs="auto">
-                        <img src={social_twitter} alt="social_twitter"/>
-                    </Col>
-                    <Col xs="auto">
-                        <img src={social_discord} alt="social_discord"/>
-                    </Col>
-                </Row>
+                <Container>
+                    <Row>
+                        <Col xs="auto">
+                            <img src={social_facebook} alt="social_facebook"/>
+                        </Col>
+                        <Col xs="auto">
+                            <img src={social_instagram} alt="social_instagram"/>
+                        </Col>
+                        <Col xs="auto">
+                            <img src={social_twitter} alt="social_twitter"/>
+                        </Col>
+                        <Col xs="auto">
+                            <img src={social_discord} alt="social_discord"/>
+                        </Col>
+                    </Row>
+                </Container>
                 <br/>
-                <hr />
+                <hr/>
             </div>
         </div>
     )
